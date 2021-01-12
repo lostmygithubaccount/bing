d2e9c451a35477927e977675ae9d33e5e10a47ef
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-01-12 13:22:27 -0800

    saving

---
+++ src/prep.py
@@ -0,0 +1,56 @@
+# imports
+import mlflow
+import argparse
+
+import pandas as pd
+import dask.dataframe as dd
+
+from distributed import Client
+
+if __name__ == "__main__":
+
+    # argparse setup
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", type=str)
+    parser.add_argument("--output", type=str, default="cody/dask/out/train.parquet")
+    parser.add_argument("--npartitions", type=int, default=256)
+    args = parser.parse_args()
+
+    # distributed setup
+    print("initializing...")
+    c = Client()
+    print(c)
+
+    # get data
+    from azureml.core import Run
+
+    run = Run.get_context()
+    ws = run.experiment.workspace
+    ds = ws.get_default_datastore()
+    container_name = ds.container_name
+    storage_options = {"account_name": ds.account_name, "account_key": ds.account_key}
+
+    print(ws)
+    print(ds)
+    print(f"az://{container_name}/{args.input}")
+
+    # read into dataframes
+    print("creating dataframes...")
+    df = dd.read_table(
+        f"az://{container_name}/{args.input}",
+        storage_options=storage_options,
+        blocksize=5e9,
+    )
+    print(df)
+
+    # data processing
+    print("processing data...")
+
+    # writing data
+    print("writing data...")
+    df.repartition(npartitions=args.npartitions).to_parquet(
+        f"az://{container_name}/{args.output}",
+        storage_options=storage_options,
+        # overwrite=True,
+        write_index=False,
+    )
